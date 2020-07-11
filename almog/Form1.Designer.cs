db658b9dfde573ffa1fc1d6219bbea5ddf6b37b1
Almog <almogb001@gmail.com>
2020-07-11 12:39:23 +0300

    Add project files.

---
+++ almog/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace almog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCols = new System.Windows.Forms.TextBox();
+            this.EnterMatrixSizeLable = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.empty = new System.Windows.Forms.Label();
+            this.txtRotate = new System.Windows.Forms.TextBox();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.lblRotateMatrix = new System.Windows.Forms.Label();
+            this.lblRotateMatrix2 = new System.Windows.Forms.Label();
+            this.lblFill2 = new System.Windows.Forms.Label();
+            this.lblFill1 = new System.Windows.Forms.Label();
+            this.btnFill = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnReflectUpDown = new System.Windows.Forms.Button();
+            this.btnReflectRightLeft = new System.Windows.Forms.Button();
+            this.lblreflect = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCols
+            // 
+            this.txtCols.Location = new System.Drawing.Point(203, 4);
+            this.txtCols.Name = "txtCols";
+            this.txtCols.Size = new System.Drawing.Size(35, 20);
+            this.txtCols.TabIndex = 1;
+            this.txtCols.Text = "0";
+            // 
+            // EnterMatrixSizeLable
+            // 
+            this.EnterMatrixSizeLable.AutoSize = true;
+            this.EnterMatrixSizeLable.Location = new System.Drawing.Point(16, 7);
+            this.EnterMatrixSizeLable.Name = "EnterMatrixSizeLable";
+            this.EnterMatrixSizeLable.Size = new System.Drawing.Size(139, 13);
+            this.EnterMatrixSizeLable.TabIndex = 2;
+            this.EnterMatrixSizeLable.Text = "Enter matrix size (rows, cols)";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(246, 3);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(87, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create Matrix";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // empty
+            // 
+            this.empty.AutoSize = true;
+            this.empty.Location = new System.Drawing.Point(28, 44);
+            this.empty.Name = "empty";
+            this.empty.Size = new System.Drawing.Size(0, 13);
+            this.empty.TabIndex = 4;
+            // 
+            // txtRotate
+            // 
+            this.txtRotate.Location = new System.Drawing.Point(614, 40);
+            this.txtRotate.Name = "txtRotate";
+            this.txtRotate.Size = new System.Drawing.Size(36, 20);
+            this.txtRotate.TabIndex = 8;
+            this.txtRotate.Text = "90";
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(712, 39);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(75, 23);
+            this.btnRotate.TabIndex = 9;
+            this.btnRotate.Text = "Rotate";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
+            // lblRotateMatrix
+            // 
+            this.lblRotateMatrix.AutoSize = true;
+            this.lblRotateMatrix.Location = new System.Drawing.Point(531, 43);
+            this.lblRotateMatrix.Name = "lblRotateMatrix";
+            this.lblRotateMatrix.Size = new System.Drawing.Size(83, 13);
+            this.lblRotateMatrix.TabIndex = 8;
+            this.lblRotateMatrix.Text = "Rotate matrix by";
+            // 
+            // lblRotateMatrix2
+            // 
+            this.lblRotateMatrix2.AutoSize = true;
+            this.lblRotateMatrix2.Location = new System.Drawing.Point(651, 43);
+            this.lblRotateMatrix2.Name = "lblRotateMatrix2";
+            this.lblRotateMatrix2.Size = new System.Drawing.Size(45, 13);
+            this.lblRotateMatrix2.TabIndex = 9;
+            this.lblRotateMatrix2.Text = "degrees";
+            // 
+            // lblFill2
+            // 
+            this.lblFill2.AutoSize = true;
+            this.lblFill2.Location = new System.Drawing.Point(653, 10);
+            this.lblFill2.Name = "lblFill2";
+            this.lblFill2.Size = new System.Drawing.Size(16, 13);
+            this.lblFill2.TabIndex = 13;
+            this.lblFill2.Text = "to";
+            // 
+            // lblFill1
+            // 
+            this.lblFill1.AutoSize = true;
+            this.lblFill1.Location = new System.Drawing.Point(456, 10);
+            this.lblFill1.Name = "lblFill1";
+            this.lblFill1.Size = new System.Drawing.Size(159, 13);
+            this.lblFill1.TabIndex = 12;
+            this.lblFill1.Text = "Auto fill matrix with numbers from";
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(712, 6);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(75, 23);
+            this.btnFill.TabIndex = 5;
+            this.btnFill.Text = "Fill";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(670, 7);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(36, 20);
+            this.txtTo.TabIndex = 4;
+            this.txtTo.Text = "0";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(614, 7);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(36, 20);
+            this.txtFrom.TabIndex = 3;
+            this.txtFrom.Text = "0";
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(162, 4);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(35, 20);
+            this.txtRows.TabIndex = 0;
+            this.txtRows.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.lblreflect);
+            this.panel1.Controls.Add(this.btnReflectRightLeft);
+            this.panel1.Controls.Add(this.btnReflectUpDown);
+            this.panel1.Controls.Add(this.btnCreate);
+            this.panel1.Controls.Add(this.txtFrom);
+            this.panel1.Controls.Add(this.txtRows);
+            this.panel1.Controls.Add(this.lblFill2);
+            this.panel1.Controls.Add(this.txtCols);
+            this.panel1.Controls.Add(this.lblFill1);
+            this.panel1.Controls.Add(this.EnterMatrixSizeLable);
+            this.panel1.Controls.Add(this.btnFill);
+            this.panel1.Controls.Add(this.txtRotate);
+            this.panel1.Controls.Add(this.txtTo);
+            this.panel1.Controls.Add(this.btnRotate);
+            this.panel1.Controls.Add(this.lblRotateMatrix2);
+            this.panel1.Controls.Add(this.lblRotateMatrix);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(834, 65);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(0, 546);
+            this.panel2.TabIndex = 15;
+            // 
+            // btnReflectUpDown
+            // 
+            this.btnReflectUpDown.Location = new System.Drawing.Point(88, 34);
+            this.btnReflectUpDown.Name = "btnReflectUpDown";
+            this.btnReflectUpDown.Size = new System.Drawing.Size(75, 23);
+            this.btnReflectUpDown.TabIndex = 6;
+            this.btnReflectUpDown.Text = "Up/Down";
+            this.btnReflectUpDown.UseVisualStyleBackColor = true;
+            this.btnReflectUpDown.Click += new System.EventHandler(this.btnReflectUp_Click);
+            // 
+            // btnReflectRightLeft
+            // 
+            this.btnReflectRightLeft.Location = new System.Drawing.Point(170, 34);
+            this.btnReflectRightLeft.Name = "btnReflectRightLeft";
+            this.btnReflectRightLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnReflectRightLeft.TabIndex = 7;
+            this.btnReflectRightLeft.Text = "Right/Left";
+            this.btnReflectRightLeft.UseVisualStyleBackColor = true;
+            this.btnReflectRightLeft.Click += new System.EventHandler(this.btnReflectRightLeft_Click);
+            // 
+            // lblreflect
+            // 
+            this.lblreflect.AutoSize = true;
+            this.lblreflect.Location = new System.Drawing.Point(16, 39);
+            this.lblreflect.Name = "lblreflect";
+            this.lblreflect.Size = new System.Drawing.Size(72, 13);
+            this.lblreflect.TabIndex = 18;
+            this.lblreflect.Text = "Reflect Matrix";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(830, 600);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(834, 611);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.empty);
+            this.MinimumSize = new System.Drawing.Size(850, 650);
+            this.Name = "Form1";
+            this.Text = "Matrix Rotate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtCols;
+        private System.Windows.Forms.Label EnterMatrixSizeLable;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label empty;
+        private System.Windows.Forms.TextBox txtRotate;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.Label lblRotateMatrix;
+        private System.Windows.Forms.Label lblRotateMatrix2;
+        private System.Windows.Forms.Label lblFill2;
+        private System.Windows.Forms.Label lblFill1;
+        private System.Windows.Forms.Button btnFill;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnReflectUpDown;
+        private System.Windows.Forms.Label lblreflect;
+        private System.Windows.Forms.Button btnReflectRightLeft;
+    }
+}
+
---
+++ almog/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace almog
+{
+    public partial class Form1 : Form
+    {
+        //TODO
+        // Add support for MxN matrix. as M!=N
+            // (1) update Tranpose 
+            // (2) cancele rows == cols check when building a new matrix 
+
+        // Change these in order to define matrix (x,y) starting point and spaces between cells
+        public const int X_START = 10;
+        public const int Y_START = 10;
+        public const int X_SPACER = 40;
+        public const int Y_SPACER = 40;
+        public const int WIDTH = 25;
+        public const int HEIGHT = 25;
+        public bool transposeFlage = false;
+        public int rows = 0, cols = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtRotate.Enabled = false;
+            lblRotateMatrix.ForeColor = Color.FromArgb(211, 211, 211);
+            lblRotateMatrix2.ForeColor = Color.FromArgb(211, 211, 211);
+            btnRotate.Enabled = false;
+
+            lblFill1.ForeColor = Color.FromArgb(211, 211, 211);
+            lblFill2.ForeColor = Color.FromArgb(211, 211, 211);
+            txtFrom.Enabled = false;
+            txtTo.Enabled = false;
+            btnFill.Enabled = false;
+
+            lblreflect.ForeColor = Color.FromArgb(211, 211, 211);
+            btnReflectRightLeft.Enabled = false;
+            btnReflectUpDown.Enabled = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //making sure user cannot input a none numeric char to a textbox
+            txtCols.KeyPress += Txt_KeyPress_Check;
+            txtRows.KeyPress += Txt_KeyPress_Check;
+            txtFrom.KeyPress += Txt_KeyPress_Check;
+            txtTo.KeyPress += Txt_KeyPress_Check;
+            txtRotate.KeyPress += Txt_KeyPress_Check;
+        }
+
+        private void Txt_KeyPress_Check(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(Char.IsNumber(e.KeyChar) || e.KeyChar == '\b' || e.KeyChar == '-');
+        }
+
+        public void reversCol()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols / 2; j++)
+                {
+                    //temp <- end
+                    String temp = panel2.Controls[$"{i}-{cols-j-1}"].Text.ToString();
+
+                    //end <- start
+                    panel2.Controls[$"{i}-{cols - j - 1}"].Text = panel2.Controls[$"{i}-{j}"].Text;
+
+                    //start <- temp
+                    panel2.Controls[$"{i}-{j}"].Text = temp;
+                }
+            }
+        }
+
+        public void reversRow()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols / 2; j++)
+                {
+                    //temp <- end
+                    String temp = panel2.Controls[$"{cols - j - 1}-{i}"].Text.ToString();
+
+                    //end <- start
+                    panel2.Controls[$"{cols - j - 1}-{i}"].Text = panel2.Controls[$"{j}-{i}"].Text;
+
+                    //start <- temp
+                    panel2.Controls[$"{j}-{i}"].Text = temp;
+                }
+            }
+        }
+
+        public void transportMatrix()
+        {
+            
+            string[,] arr = new string[rows, cols];
+
+            //fill arr with current matrix values 
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    arr[i, j] = panel2.Controls[$"{i}-{j}"].Text.ToString();
+                }
+            }
+
+            //update matrix with transposed values 
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    panel2.Controls[$"{j}-{i}"].Text = arr[i, j];
+                    //this.Controls.Find("txt" + Convert.ToInt32(i) + Convert.ToInt32(j), true)[0].Text = arr[j, i];
+                }
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if((Convert.ToInt32(txtRows.Text)) <= 0 || Convert.ToInt32(txtCols.Text) <= 0)
+            {
+                MessageBox.Show("Invalid matrix size. rows & cols number should be higher than 1");
+            }
+            else if ((Convert.ToInt32(txtRows.Text)) != Convert.ToInt32(txtCols.Text))
+            {
+                MessageBox.Show("Currently supporting only a NxN matrix");
+            } else
+            {
+                txtRotate.Enabled = true;
+                btnRotate.Enabled = true;
+                lblRotateMatrix.ForeColor = Color.FromArgb(0, 0, 0);
+                lblRotateMatrix2.ForeColor = Color.FromArgb(0, 0, 0);
+
+                lblFill1.ForeColor = Color.FromArgb(0, 0, 0);
+                lblFill2.ForeColor = Color.FromArgb(0, 0, 0);
+                txtFrom.Enabled = true;
+                txtTo.Enabled = true;
+                btnFill.Enabled = true;
+
+                lblreflect.ForeColor = Color.FromArgb(0, 0, 0);
+                btnReflectRightLeft.Enabled = true;
+                btnReflectUpDown.Enabled = true;
+
+                panel2.Controls.Clear();
+                rows = Convert.ToInt32(txtRows.Text);
+                cols = Convert.ToInt32(txtCols.Text);
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        var tb = new TextBox()
+                        {
+                            Name = $"{col}-{row}",
+                            Width = WIDTH,
+                            Text = "0",
+                            Location = new Point(col * (WIDTH + 2) + 10, row * (HEIGHT + 2))
+                        };
+                        tb.KeyPress += Txt_KeyPress_Check;
+                        panel2.Controls.Add(tb);
+                    }
+                }
+                return;
+            }
+        }
+
+        private void btnRotate_Click(object sender, EventArgs e)
+        {
+            if ((int.Parse(txtRotate.Text)) % 90 != 0) 
+            {
+                MessageBox.Show("Matrix rotation allowed with multiplications of +-90 only.");
+            } else
+            {
+                var rot = int.Parse(txtRotate.Text);
+                if (rot == 0) return;
+                if (Math.Abs(rot) > 360) rot = rot % 360;
+
+                if (rot > 0)
+                {
+                    while(rot > 0)
+                    {
+                        rotate90Clockwise();
+                        rot = rot - 90;
+                    }
+                    
+                }
+                else
+                {
+                    while(rot < 0)
+                    {
+                        rotate90AntiClockwise();
+                        rot = rot + 90;
+                    }
+                }
+
+            }
+        }
+
+        private void rotate90Clockwise()
+        {
+            transportMatrix();
+            reversRow();
+        }
+
+        private void rotate90AntiClockwise()
+        {
+            transportMatrix();
+            reversCol();
+        }
+
+        private void btnReflectUp_Click(object sender, EventArgs e)
+        {
+            reversCol();
+        }
+
+        private void btnReflectRightLeft_Click(object sender, EventArgs e)
+        {
+            reversRow();
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            if((int.Parse(txtFrom.Text)) > int.Parse(txtTo.Text))
+            {
+                MessageBox.Show("Invalid range. Start index should be lower to End index.");
+            }
+            else
+            {
+                Random r = new Random();
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        var ctrl = (TextBox)panel2.Controls[$"{col}-{row}"];
+                        ctrl.Text = r.Next(int.Parse(txtFrom.Text), int.Parse(txtTo.Text)).ToString();
+                    }
+                }
+            }
+        }
+
+
+    }
+}
